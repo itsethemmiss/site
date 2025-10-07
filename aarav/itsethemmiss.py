14042afa8169691c941ce2615226b8e2602f66a2
Ethem Beldagli <198623801+fdsfasdf2314@users.noreply.github.com>
2025-10-07 11:17:02 +0100

    Delete aarav directory

--- aarav/itsethemmiss.py
+++
@@ -1,9 +0,0 @@
-ethem = input("What type of ethem miss do you want: ").lower()
-if ethem == "miss":
-    print("\nIts ethem miss")
-elif ethem == "sir":
-    print("\nIts ethem sir")
-elif ethem == "beldagli":
-    print("Its ethem boldegus beldagus")
-else:
-    print("\nWe do not support that ethem miss")
